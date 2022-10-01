50dbea586ad4026ad9c06cecaddc0fac47b840a4
Nick Bryan <nickbryan.dev@gmail.com>
2022-10-01 11:56:51 +0100

    Clean up go setup

--- nvim/lua/go_setup.lua
+++ nvim/lua/go_setup.lua
@@ -1,25 +1,2 @@
-local util = require "lspconfig/util"
-require'lspconfig'.gopls.setup({
-    cmd = {"gopls", "serve"},
-    filetypes = {"go", "gomod"},
-    root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-    settings = {
-        gopls = {
-            analyses = {
-                fieldalignment = true,
-                nilness = true,
-                shadow = true,
-                unusedparams = true,
-                unusedwrite = true,
-                useany = true,
-            },
-            gofumpt = true,
-            usePlaceholders = true,
-            experimentalPostfixCompletions = true,
-            staticcheck = true,
-        },
-    },
-})
-
-require('go').setup()
+require('go').setup({lsp_cfg = true})
 
