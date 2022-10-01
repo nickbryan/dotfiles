0365e63a53821f361226f97fd8918229ced75dcd
Nick Bryan <nickbryan.dev@gmail.com>
2022-10-01 11:31:00 +0100

    Fix go

--- nvim/lua/go_setup.lua
+++ nvim/lua/go_setup.lua
@@ -1,2 +1,25 @@
+local util = require "lspconfig/util"
+require'lspconfig'.gopls.setup({
+    cmd = {"gopls", "serve"},
+    filetypes = {"go", "gomod"},
+    root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+    settings = {
+        gopls = {
+            analyses = {
+                fieldalignment = true,
+                nilness = true,
+                shadow = true,
+                unusedparams = true,
+                unusedwrite = true,
+                useany = true,
+            },
+            gofumpt = true,
+            usePlaceholders = true,
+            experimentalPostfixCompletions = true,
+            staticcheck = true,
+        },
+    },
+})
+
 require('go').setup()
 
