da6c60b5c6ebb2b02b711d97aa81a7e9b6a876ee
Nick Bryan <nickbryan.dev@gmail.com>
2024-09-04 21:08:52 +0100

    Add and configure telescope for navigatin and search

--- nvim/lua/nbconfig/plugins/telescope.lua
+++ nvim/lua/nbconfig/plugins/telescope.lua
@@ -1,12 +1,15 @@
-local builtin = require('telescope.builtin')
+local builtin = require("telescope.builtin")
 
 return {
     url = "https://github.com/nvim-telescope/telescope.nvim",
     tag = "0.1.8",
     dependencies = { 
-        url = 'https://github.com/nvim-lua/plenary.nvim',
+        { url = "https://github.com/nvim-lua/plenary.nvim" },
+        { url = "https://github.com/nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     },
     keys = {
-        { '<leader>pf', function() require('telescope.builtin').find_files() end, { mode = 'n', noremap = true, } }, 
+        { "<leader>pf", function() require("telescope.builtin").find_files({ hidden = true }) end, mode = "n", noremap = true, desc = "Search for project files" },
+        { "<leader>ps", function() require("telescope.builtin").live_grep() end, mode = "n", noremap = true, desc = "Search for string in project files" },
+        { "<leader>e", function() require("telescope.builtin").oldfiles({ cwd_only = true }) end, mode = "n", noremap = true, desc = "Search for previously opened files" },
     },
 }
