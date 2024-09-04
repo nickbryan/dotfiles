0319bed73187378cbe617ae2a7ff82586e277514
Nick Bryan <nickbryan.dev@gmail.com>
2024-09-04 21:10:41 +0100

    Add basic remappings

--- nvim/lua/nbconfig/remap.lua
+++ nvim/lua/nbconfig/remap.lua
@@ -2,5 +2,18 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- Disable arrow keys
+vim.keymap.set({ "n", "i" }, "<Up>", "", { noremap = true })
+vim.keymap.set({ "n", "i" }, "<Down>", "", { noremap = true })
+vim.keymap.set({ "n", "i" }, "<Left>", "", { noremap = true })
+vim.keymap.set({ "n", "i" }, "<Right>", "", { noremap = true })
+
+-- Left and right to switch buffers
+vim.keymap.set("n", "<Left>", ":bp<CR>", { noremap = true, desc = "Previous buffer" })
+vim.keymap.set("n", "<Right>", ":bn<CR>", { noremap = true, desc = "Next buffer" })
+
+-- Exit insert mode
+vim.keymap.set("i", "jj", "<Esc>", { noremap = true, desc = "Exit insert mode" })
+
 -- Navigation
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Project view - netrw" })
