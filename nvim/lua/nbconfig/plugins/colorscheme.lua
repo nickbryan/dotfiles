178c3caed7ca7ff5192b1e15ff3a86a05228a67e
Nick Bryan <nickbryan.dev@gmail.com>
2024-09-05 09:33:10 +0100

    Add tokyonight colorscheme as default

---
+++ nvim/lua/nbconfig/plugins/colorscheme.lua
@@ -0,0 +1,9 @@
+return {
+    url = "https://github.com/folke/tokyonight.nvim",
+    tag = "v4.8.0",
+    lazy = false,
+    priority = 1000,
+    config = function ()
+        vim.cmd([[colorscheme tokyonight-night]])
+    end,
+}
