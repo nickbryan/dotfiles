0863b420deb295d2be51ba17821057fc77c0316d
Nick Bryan <nickbryan.dev@gmail.com>
2024-09-19 21:04:44 +0100

    Add telescope ui select for code actions

--- nvim/lua/nbconfig/plugins/telescope.lua
+++ nvim/lua/nbconfig/plugins/telescope.lua
@@ -2,19 +2,10 @@ return {
     {
         url = "https://github.com/nvim-telescope/telescope.nvim",
         dependencies = {
-            {
-                url = "https://github.com/nvim-lua/plenary.nvim",
-            },
-            {
-                url = "https://github.com/nvim-telescope/telescope-fzf-native.nvim",
-                build = "make",
-                config = function()
-                    require("telescope").load_extension("fzf")
-                end,
-            },
-            {
-                url = "https://github.com/nvim-tree/nvim-web-devicons",
-            },
+            { url = "https://github.com/nvim-lua/plenary.nvim" },
+            { url = "https://github.com/nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+            { url = "https://github.com/nvim-tree/nvim-web-devicons" },
+            { url = "https://github.com/nvim-telescope/telescope-ui-select.nvim" },
         },
         keys = {
             { "<leader>pf", function() require("telescope.builtin").find_files({ hidden = true }) end, mode = "n", noremap = true, desc = "Search for project files" },
@@ -22,10 +13,20 @@ return {
             { "<leader>e",  function() require("telescope.builtin").oldfiles({ cwd_only = true }) end, mode = "n", noremap = true, desc = "Search for previously opened files" },
             { "<leader>vh", function() require("telescope.builtin").help_tags() end,                   mode = "n", noremap = true, desc = "Vim help tags" },
         },
-        opts = {
-            defaults = {
-                file_ignore_patterns = { "%.git/" },
-            },
-        },
+        config = function()
+            local telescope = require("telescope")
+            telescope.setup({
+                defaults = {
+                    file_ignore_patterns = { "%.git/" },
+                },
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown(),
+                    },
+                },
+            })
+            telescope.load_extension("fzf")
+            telescope.load_extension("ui-select")
+        end,
     },
 }
